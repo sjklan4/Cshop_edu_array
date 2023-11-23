2e16fc32c07f1921655fa8177e3e806b27a1ee2c
sjklan4 <dpeltms4@gmail.com>
2023-11-23 10:44:43 +0900

    freach문 추가

--- arraypratice/arraypratice/Program.cs
+++ arraypratice/arraypratice/Program.cs
@@ -13,5 +13,19 @@ class arrayOPer
             }
             Console.WriteLine();
         }
+
+        string[] names = { "C#", "ASP.NET" };
+        string[] names1 = { "C#", "Windows Forms", "ASP.NET" };
+        for (int k = 0; k < names1.Length; k++) //배열 길이만큼 반복 C#  Windows Forms  ASP.NET 
+        {
+            Console.WriteLine(names1[k]); 
+        }
+
+        Console.WriteLine("");
+
+        foreach (var name in names1) // 배연 안에 값이 있으면 출력 반복  C#  Windows Forms  ASP.NET
+        {
+            Console.WriteLine(name);
+        }
     }
 }
