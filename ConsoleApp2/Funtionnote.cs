93ba0f73c2f357101094cc870d56abadfa2fab1d
sjklan4 <dpeltms4@gmail.com>
2023-11-23 18:39:49 +0900

    1123function

---
+++ ConsoleApp2/Funtionnote.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class FunctionNote
+{
+    // sum함수 선언 - 큰 의미 없으면 체이닝?
+    // static void Sum() => Console.WriteLine("합계 :  "); 
+    static void Sum(int first, int second) // 넘겨 주는 값  
+    { 
+        int sum = first + second;
+        Console.WriteLine($"합계 :{sum}");
+    }
+
+   
+        
+    
+    // static void Main() => Sum(값,값); 방식으로 사용 가능 
+    static void Main(string[] args)
+        {
+           FunctionNote.Sum(3,5);
+        }
+}
---
+++ functionOP/function.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class FunctionDemo 
+{
+    static void Show()
+    {
+        Console.WriteLine("확인용");
+    }
+
+    static void arraytest()
+    {
+        int[] numbers = { 1, 2, 3 };
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+
+    static void Main() 
+    {
+        arraytest();
+        Show();
+    }
+
+}
