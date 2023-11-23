037b72501c4087d50acd6e4d9d8fdf49f963018b
sjklan4 <dpeltms4@gmail.com>
2023-11-23 10:27:18 +0900

    1123 arrayrepeat

---
+++ arraypratice/arraypratice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class arrayOPer
+{
+    static void Main()
+    {
+        int[,] array = { { 1, 2, 3, 4, }, { 5, 6, 7, 8 } };
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j]}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
