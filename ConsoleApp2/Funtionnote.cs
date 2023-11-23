38b31eff7c8c83c53b43ddaba1d299658c9852b7
sjklan4 <dpeltms4@gmail.com>
2023-11-24 00:16:41 +0900

    functionPR

--- ConsoleApp2/Funtionnote.cs
+++ ConsoleApp2/Funtionnote.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Diagnostics;
 
 class FunctionNote
 {
     // sum함수 선언 - 큰 의미 없으면 체이닝?
     // static void Sum() => Console.WriteLine("합계 :  "); 
-    static void Sum(int first, int second) // 넘겨 주는 값  
+    static int Sum(int first, int second) // 넘겨 주는 값  
     { 
+        
         int sum = first + second;
-        Console.WriteLine($"합계 :{sum}");
+        return sum;
+        //Console.WriteLine($"합계 :{sum}");
     }
 
    
@@ -16,6 +19,13 @@ class FunctionNote
     // static void Main() => Sum(값,값); 방식으로 사용 가능 
     static void Main(string[] args)
         {
-           FunctionNote.Sum(3,5);
+            int result = Sum(3, 5);
+            Console.WriteLine(result);
+           //FunctionNote.Sum(3,5);
         }
+    //위 함수 부분을 별도로 체이닝 식으로 변경 가능
+
+    //static int Sum(int first, int second) => (first + second);
+
+    //static void Main() => Console.WriteLine(Sum(3,5));
 }
---
+++ FunctionPr/functionpr.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class FunctionCall
+{
+    //static void Hi()
+    //{
+    //    Console.WriteLine("안녕하세요");
+
+    //}
+
+    //static void Main()
+    //{ 
+    //    Hi(); Hi(); Hi(); Hi();
+    //}
+
+
+
+
+    // FunctionParameter.cs
+    // 매개 변수(Parameter)가 있는 함수 만들고 호출하기 
+
+    //static void ShowMessage(string message)
+    //{ 
+    //    Console.WriteLine(message); 
+
+    //}
+
+    //static void Main() 
+    //{
+    //    ShowMessage("매개 변수");
+    //    ShowMessage("Parameter");
+    //}
+
+    //static string Getstring() 
+    //{
+    //    return "반환값(Return Value)";
+    //}
+
+    //static void Main()
+    //{ 
+    //    string retunrValue = Getstring();
+    //    Console.WriteLine(retunrValue);
+    //}
+
+    // FunctionReturnValue.cs
+    // 반환값이 있는 함수(메서드)
+
+    static int SquareFuntion(int x)
+    {
+        int r = x * x;
+        return r;
+    
+    }
+
+    static void Main()
+    {
+        int r = SquareFuntion(2);
+        Console.WriteLine(r);
+    }
+
+}
