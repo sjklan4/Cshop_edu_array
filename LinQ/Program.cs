f63a6dac0f8e5aa1cc6344b63fe1f8ac946c7271
sjklan4 <dpeltms4@gmail.com>
2023-12-07 17:51:03 +0900

    1207

---
+++ LinQ/Program.cs
@@ -0,0 +1,19 @@
+﻿// LINQ : C#언어에 직접 쿼리 기능을 통합하는 방식을 기반으로 하는 기술 집합 이름
+// 미리 만들어져 있는 Method들을 사용할 수 있게 해주는 기능
+using System;
+using System.Linq;
+
+class Linqsum
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3 };
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+        Console.WriteLine(sum);
+
+    }
+}
