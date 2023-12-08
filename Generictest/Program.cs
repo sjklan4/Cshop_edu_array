f6711b554190e8e24ad8cd37dbfb75ace8dd7b39
sjklan4 <dpeltms4@gmail.com>
2023-12-08 13:15:04 +0900

    1208

---
+++ Generictest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Transactions;
+
+class MyStack<T>
+{
+    T[] elements;
+    int pos = 0;
+
+    public MyStack()
+    {
+        elements = new T[100];
+    }
+
+    public void Push(T element)
+    {
+        elements[++pos] = element;
+    }
+
+    public T Pop()
+    {
+        return elements[pos--];
+    }
+
+}
+class Program
+{
+    static void Main()
+    {
+        MyStack<int> numberStack = new MyStack<int>();
+        MyStack<string> nameStack = new MyStack<string>();
+
+        // 스택에 값 푸시
+        numberStack.Push(101);
+        numberStack.Push(42);
+        nameStack.Push("John Doe");
+
+        // 스택에서 값 팝
+        int poppedNumber1 = numberStack.Pop();
+        int poppedNumber = numberStack.Pop();
+        string poppedName = nameStack.Pop();
+
+        // 콘솔에 출력
+        Console.WriteLine(poppedNumber1);
+        Console.WriteLine("Popped Number: " + poppedNumber);
+        Console.WriteLine("Popped Name: " + poppedName);
+    }
+}
+// 두 개의 서로 다른 타입을 갖는 스택 객체를 생성
+//MyStack<int> numberStack = new MyStack<int>();
+//MyStack<string> nameStack = new MyStack<string>();
