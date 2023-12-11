2ce0951e06b4a9abd9fe710935024527f154c9c9
sjklan4 <dpeltms4@gmail.com>
2023-12-11 18:16:29 +0900

    1211buble

---
+++ BubleShort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+class BubleSort
+{
+    
+
+
+    static void Main() 
+    {
+        int[] data = { 3, 2, 1, 5, 4 };
+        Array.Sort(data);
+        Console.WriteLine(string.Join(", ", data));
+        
+
+        Array.Reverse(data);
+        Console.WriteLine(string.Join(", ", data));
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            for (int j = 0; j < data.Length; j++) 
+            {
+                if (data[i] > data[j])
+                { 
+                    int temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
+                }
+            }
+
+        }
+
+    }
+}
