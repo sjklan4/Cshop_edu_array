9dd43c8bb41debabcd36a7b163ee7bc32d478cae
sjklan4 <dpeltms4@gmail.com>
2023-12-12 22:58:33 +0900

    1212

---
+++ MaxAlgorithm/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+class MaxAlgorithm
+{
+    static void Main()
+    {
+        // Intitialzie
+
+        // input
+        int[] numbers = { -2, -5, -3, -7, -1 };
+        // process :MAx
+
+
+
+
+        // Output
+        Console.WriteLine( $"쵀댓값 : {numbers.Max()}"); //Linq방식
+
+        //문 사용법
+        int max = int.MinValue; // 정수형 데이터 중 가장 작은 값으로 초기화
+
+        int[] numbers1 = { 1, 2, 3, 4, 5, 6, 7 };
+
+        for (int i = 0; i < numbers1.Length; i++)
+        {
+            if (numbers1[i] > max)
+            {
+                max = numbers1[i]; // MAX알고리즘으로 더큰 값으로 할당
+            }
+        }
+        Console.WriteLine($"최대 값은? : {max}");
+
+
+    }
+}
--- SumAlgorithm/Program.cs
+++ SumAlgorithm/Program.cs
@@ -5,18 +5,20 @@ class SumAlgorithm
 {
     static void Main()
     {
+        //조건
         int[] scores = { 100, 75, 50, 37, 90, 95 };
         int sum = 0;
 
+        // process
         for (int i = 0; i < scores.Length; i++) //int배열에 있는 6개 를 반복해서 실행
         {
             if (scores[i] >= 80)  // 만약 scores값이 80이상이면 sum구문 실행 다시 위 for문을 구동 - 80인값 +  반복
             {
                 sum += scores[i];
             }
-
-
         }
+        
+        // output
         Console.WriteLine(sum); //최종 출력
         Console.WriteLine("");
         ////Linq? 가능?
