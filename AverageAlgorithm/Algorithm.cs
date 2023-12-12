3054b270c4de15a91b19614cc146477ed11067af
sjklan4 <dpeltms4@gmail.com>
2023-12-12 18:36:46 +0900

    1212

---
+++ AverageAlgorithm/Algorithm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+class AverageAlgorith 
+{
+    static void Main()
+    {
+        int[] data = { 90, 65, 78, 50, 95 };
+        int sum = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (data[i] >= 80 && data[i] <= 95) 
+            { 
+                
+                sum += data[i];
+                cnt++;
+            }
+
+        }
+        
+        double avg = (double)sum / cnt;
+        Console.WriteLine(avg.ToString("F2"));
+
+        // linq사용시 암시적 형식에 대한 것을 미리 설정하지 않으면 구동 되지 않음. 아래 구문은int로 표현이 안되는 소수점을 포함한 수기 때문에 
+        // double로 변수 선언 해야됨
+        double linqavg = (new int[] { 50, 65, 78, 90, 95 }).Where( d => d >= 80 && d <= 95 ).Average();
+        Console.WriteLine(linqavg);
+    }
+}
--- CountAlgorithm1/Program.cs
+++ CountAlgorithm1/Program.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 
 class ConutAlgorithm
 {
     static void Main()
     {
-        int count = Enumerable.Range(1,1000).Where(n => n % 13 == 0).Count();
+        int count = Enumerable.Range(1,1000).Where(n => n % 13 == 0).Count(); //linq형식
                 
 
         Console.WriteLine(count);
         Console.WriteLine("----------------------------------------------------");
+        
+        int cnt = 0;
+        for (int i = 1; i <= 1000; i++)
+        {
+            
+            if (i % 13 == 0) 
+            {
+                cnt++;
+            }
 
+            
+        }
+        Console.WriteLine("13의 배수개수: " + cnt);
+
+    
         
 
         //아래 sumalgorithm 빌드 문제로 예시 옮겨서 실행
