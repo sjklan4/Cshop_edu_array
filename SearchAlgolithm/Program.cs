dc50b8dbc7b74f9a7786ed0d6c8698bebca54b78
sjklan4 <dpeltms4@gmail.com>
2023-12-14 18:28:40 +0900

    1214141414

---
+++ SearchAlgolithm/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+//주어진 데이터에서 특정 데이터 찾기
+class SearchAlgorithm 
+{
+    static void Main() 
+    {
+        // Input
+        int[] data = { 1, 3, 5, 7, 9 }; // 오름차순으로 정렬되었다고 가정
+        int N = data.Length; //의사코드
+        int search = 9; // 검색할 데이터
+        bool flag = false; // 플래그 변수 : 찾으면 true 못찾으면 false
+        int index = -1; // 인덱스 변수 찾은 위치
+
+        // Process : full scan - > index Scan
+        int low = 0; //min: 낮은 인덱스
+        int high = N - 1;
+        while (low <= high)
+        { 
+            int mid = (low + high) / 2; // 중간 인덱스 구하기
+            if (data[mid] == search)
+            { 
+                flag = true;
+                index = mid;
+                break;
+            }
+            if (data[mid] > search)
+            {
+                high = mid - 1; //찾을 데이터가 작으면 왼쪽 영역으로 이동 
+            }
+            else 
+            { 
+                low = mid + 1; //찾을 데이터가 크면 오른쪽 영역으로 이동
+            }
+
+        }
+
+
+
+        // Output
+        if (flag)
+        {
+            Console.WriteLine($"{search}를 {index}위치에서 찾았습니다.");
+        }
+        else {
+            Console.WriteLine("찾지 못함");
+        }
+
+
+
+
+    }
+
+}
+
---
+++ SortAlgorithm/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+class SortAlgorithm 
+{
+    static void Main()
+    {
+        // Input
+        int[] data = { 3, 2, 1, 4, 5,7,8,9 };
+        int N = data.Length; //의사코드(슈도코드) 형태로 알고리즘 표현
+
+        // Process : Selection Sort(선택 정렬)
+        for (int j = 0; j < N; j++)
+        {
+            for (int k = j + 1 ; k < N; k++)
+            {
+                
+                if (data[j] < data[k]) 
+                {
+                    int tmp = data[j];
+                    data[j] = data[k];
+                    data[k] = tmp;
+                }
+            }
+            
+        }
+
+
+        //Output
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write($"{data[i]}\t");
+        }
+       
+    }
+    
+
+}
