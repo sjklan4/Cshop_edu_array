6411f701fc432e118e31b3b3b0ffab0ce7719d5b
sjklan4 <dpeltms4@gmail.com>
2023-12-15 17:58:41 +0900

    1215 class

---
+++ ClassCode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ObjectNpte 
+{
+    public class Counter
+    {
+        public void GetTodayVisitCount()
+        {
+            Console.WriteLine("확인용");
+        }
+    }
+    class ObjectNote 
+    {
+        static void Main() 
+        { 
+            Counter counter = new Counter();
+            counter.GetTodayVisitCount();
+        }
+    }
+
+}
