6c7c8549356197dae12d37afc8300108ff2f8b55
sjklan4 <dpeltms4@gmail.com>
2023-12-18 18:33:18 +0900

    1218property

---
+++ Property/property.cs
@@ -0,0 +1,26 @@
+﻿//속성 : 필드의 값을 읽거나 쓰거나 꼐산하는 방법을 제공하는 클래스의 속성을 나타내는 멥버
+// C#의 모든 멤버는 대문자로 시작
+using System;
+
+class Car 
+{
+    private string name; //필드 
+    public string Name  // 속성부분
+    { 
+        get { return name; } //name을 받고 - 선언된 private를 입력
+        set { name = value; } // Main에 셋팅된 값을 받아오는 부분
+    }
+     // 위 부분을 대체 가능한 설정  = public string Name {get; set;} = "기본 값"; 의 모양으로 기본값을 정해줄수 있다.
+    public void SetName(string name)
+    { 
+        this.name = name;
+    }
+
+    static void Main()
+    { 
+        Car car = new Car();
+        car.Name = "My Car"; // = Car car = new Car() {Name = "My Car"}; -> Object initializer
+       
+        Console.WriteLine(car.Name);
+    }
+}
---
+++ PropertyAll/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace PropertyAll 
+{
+    public class Car 
+    {
+        private string color; // 필드
+
+        public Car()   // 생성자
+        { 
+            this.color = "Black";
+        }
+
+        //메서드로 외부공개
+        public void SetColor(string color)
+        {
+            this.color = color; //this.필드 = 매개변수;
+        }
+
+        public string GetColor()
+        {
+            return color;
+        }
+
+        //속성
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            { 
+                color = value;
+            }
+
+
+        }
+        // 읽기 전용 속성
+        public string Make
+        {
+            get
+            {
+                return "벤비아제";
+            }
+
+        }
+
+        //쓰기 전용 속성
+        private string _Type;
+        public string Type
+        {
+            set
+            {
+                _Type = value;
+            }
+        }
+        public string Name { get; set; }
+    }
+    class PropertyAll
+    {
+        static void Main()
+        { 
+            // Set과 Get 메서드 사용
+            Car car1 = new Car();
+            car1.SetColor("RED");
+            Console.WriteLine(car1.GetType());
+
+            // 속성을 사용
+            Car whiteCar = new Car();
+            whiteCar.Color = "White";           //set {}
+            Console.WriteLine(whiteCar.Color);  //get {}
+
+            // 쓰기 전용 속성
+            Car car = new Car();
+            car.Type = "중형";                // 쓰기만 가능
+
+            // 축약형 속성
+            Car myCar = new Car();
+            myCar.Name = "좋은차";
+            Console.WriteLine(myCar.Name);
+        }
+    }
+
+}
