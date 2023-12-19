4bfc3716bc47fad6aff406d85a77767647b4082c
sjklan4 <dpeltms4@gmail.com>
2023-12-19 11:52:37 +0900

    1211231313123

--- delegate/Program.cs
+++ delegate/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿//대리자는 delegate 키워드를 사용
+//대리자는 함수 자체를 데이터 하나로 보고 의미 그대로 다른 메서드를 대신 실행
+//한 번에 하나 이상을 대신해서 호출할 수 있는 개념
+
+
+using System;
+
+class DelegateDemo
+{
+    static void Hi() => Console.WriteLine("안녕하세요");
+
+    delegate void SayDelegate();
+
+    static void Main()
+    {
+        SayDelegate say = Hi;
+        say();
+
+        var hi = new SayDelegate(Hi);
+        hi();
+    }
+}
---
+++ delegatetest/Program.cs
@@ -0,0 +1,15 @@
+﻿// 대리자(Delegate) : 함수 포인터, 메서드 대신 호출, 대리 운전
+using System;
+
+class DelegateSample
+{
+    static void GoForward() => Console.WriteLine("직진");
+    delegate void CarDriver(); // 대리자 형식 생성
+    static void Main()
+    {
+        GoForward(); // 내가 직접 운전
+        CarDriver goHome = new CarDriver(GoForward); // 대리 운전
+        goHome.Invoke();
+        goHome();
+    }
+}
