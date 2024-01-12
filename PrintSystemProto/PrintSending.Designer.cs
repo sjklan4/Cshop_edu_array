2d74b25d74f63a81d20d722cc3aa6037b07085b5
sjklan4 <dpeltms4@gmail.com>
2024-01-12 18:31:12 +0900

    powertsave

--- PrintSystemProto/PrintSending.Designer.cs
+++ PrintSystemProto/PrintSending.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.SuspendLayout();
             // 
             // PrintSending
@@ -43,5 +45,7 @@
         }
 
         #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
