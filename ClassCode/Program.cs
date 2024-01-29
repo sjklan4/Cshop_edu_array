9bf4445ca243be52199c16b36057581fbeac25b4
sjklan4 <dpeltms4@gmail.com>
2024-01-29 11:34:03 +0900

    임시 수정

--- ClassCode/Program.cs
+++ ClassCode/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassNamesapceCode;
+using System;
 
 namespace ObjectNpte 
 {
@@ -9,12 +10,33 @@ namespace ObjectNpte
             Console.WriteLine("확인용");
         }
     }
+
+    public partial class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public partial class Person
+    {
+        public void Print() => Console.WriteLine($"{Name} : {Age}");
+
+    }
+
+  
     class ObjectNote 
     {
         static void Main() 
         { 
             Counter counter = new Counter();
             counter.GetTodayVisitCount();
+
+            Person person = new Person();
+
+            person.Name = "C#";
+            person.Age = 29;
+
+            person.Print();
         }
     }
 
