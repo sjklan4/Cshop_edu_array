39a68d1243d7a520a9e541df12020a3991cb0598
sjklan4 <dpeltms4@gmail.com>
2024-02-22 11:08:44 +0900

    interface

---
+++ InterfaceNote/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+namespace InterfaceNote
+{
+    abstract class KS
+    {
+        public abstract void Back();
+    }
+
+    partial class MyCar : KS // 동일 이름의 클래스를 나눠서 개발 가능 - 분할 클래스 =  부분 클래스
+    {
+
+    }
+
+    partial class MyCar : KS
+    {
+        public void Right() => WriteLine("우회전");
+    }
+     //앞에 sealed를 붙이면 상속이 안되도록 막을수 있다.
+    class Car : MyCar
+    {
+        public void Run() => WriteLine("직진");
+    }
+
+    class SpyCar : Car
+    { 
+        
+    }
+    class InterfaceNote
+    {
+        
+        static void Main()
+        {
+            Car car = new Car();
+            car.Run(); car.Right();
+            SpyCar spy = new SpyCar(); spy.Run();
+        }
+    }
+}
--- MethodOV/obj/Release/net6.0/MethodOV.AssemblyInfo.cs
+++ MethodOV/obj/Release/net6.0/MethodOV.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("InterFace")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("MethodOV")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("InterFace")]
-[assembly: System.Reflection.AssemblyTitleAttribute("InterFace")]
+[assembly: System.Reflection.AssemblyProductAttribute("MethodOV")]
+[assembly: System.Reflection.AssemblyTitleAttribute("MethodOV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
