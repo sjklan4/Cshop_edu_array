aee671d273dc28a6b9f2c69139a18e12f4b6a152
sjklan4 <dpeltms4@gmail.com>
2024-03-07 22:27:47 +0900

    df2df

--- inspector_create_table.sql
+++ inspector_create_table.sql
@@ -6,3 +6,7 @@ CREATE TABLE mInpsector (
     사양 VARCHAR(500) not null
 
 );
+
+
+insert into mInpsector(차종,ALC,품번,사양)
+values('Mx5pe','1k223','kkke2','123092pr'),('Mx5peb','1dfk223','kddkke2','234092pr'),('zzc5pe','12f223','22ke2','1212pr'),('Mx35pe','1k2123','kkke12','123092pdfr')
