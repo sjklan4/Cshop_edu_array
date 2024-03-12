471ba8916bf2a5da3d02cba53f5f57ba9ecba544
sjklan4 <dpeltms4@gmail.com>
2024-03-12 10:07:08 +0900

    df2efefe123

--- reflaction/Program.cs
+++ reflaction/Program.cs
@@ -1,1 +1,31 @@
 ﻿
+
+using System;
+using System.Diagnostics;
+
+namespace AttributeDeom
+{
+    public class LuxuryAttribute : Attribute 
+    {
+        public LuxuryAttribute() => Console.WriteLine("럭셔리1!");
+    }
+    [LuxuryAttribute]
+    class Car
+    {
+        [Obsolete("다음 버전에서 제거 예정")] // 아래 있는 메서드를 더이상 하용 하지 않도록 만드는 기능 - 기존 프로그램에서 메서드를 사용 하지 않으면서 프로그램의 구조에 영향을 주지 않을수 있다.
+        public void Manual() => Console.WriteLine("수동");
+        public void Auto() => Console.WriteLine("자동 운전");
+        [Conditional("DEBUG")]public void Test() => Console.WriteLine("테스트 운전"); //DEBUG로 해당 메서드를 인식하도록 해준다.
+    }
+    class AttributeDeomo
+    {
+        static void Main()
+        { 
+            Car car = new Car();
+            car.Manual();
+            car.Auto(); // car.Manual();
+            car.Test();
+            Attribute.GetCustomAttributes(typeof(Car)); // 
+        }
+    }
+}
