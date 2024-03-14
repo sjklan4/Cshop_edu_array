6df52730e0b190f26f8e5815649f1171b72c4a46
sjklan4 <dpeltms4@gmail.com>
2024-03-14 10:07:59 +0900

    ddf23edafsadf

---
+++ PolymorphismDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PolymorphismDemo
+{
+    public abstract class Animal
+    {
+        public abstract string Cry();
+    
+    }
+
+    public class Dog : Animal
+    {
+        public override string Cry() => "짖는다.";
+        
+    }
+
+    public class Cat : Animal
+    {
+        public override string Cry() => "운다";
+
+
+    }
+
+    public class Trainer
+    {
+        public void DoCry(Animal animal)
+        { 
+            Console.WriteLine("{0}", animal.Cry());
+        
+        }
+    }
+
+    class PolymorphismDemo
+    {
+        static void Main(string[] args)
+        { 
+            
+        }
+    }
+}
