d3bb955a75f80d395f53849cd29745a299cab160
sjklan4 <dpeltms4@gmail.com>
2024-03-20 10:29:56 +0900

    dde2f2efef

---
+++ Dynamicclass/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Program
+{ 
+    public static void Main()
+
+    {
+        dynamic x;
+        x = 1_234;
+        Console.WriteLine($"{x} - {x.GetType()}");
+    }
+
+}
---
+++ Dynamicclass/Tuple.cs
@@ -0,0 +1,19 @@
+﻿// 튜플 리터럴(Tuple Literal)
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamicclass
+{
+    internal class Tuple
+    {
+
+
+    }
+}
+
